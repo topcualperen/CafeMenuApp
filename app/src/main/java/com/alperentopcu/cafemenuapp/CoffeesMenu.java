ec706c8555ba8183f6f7911402bde5a3bc7d0fcd
Alperen Topcu <alperenttpc78@gmail.com>
2022-09-23 23:18:12 +0300

    cafeMenuApplication

---
+++ app/src/main/java/com/alperentopcu/cafemenuapp/CoffeesMenu.java
@@ -0,0 +1,43 @@
+package com.alperentopcu.cafemenuapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class CoffeesMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coffees_menu);
+    }
+
+    public void americano(View view){
+
+    }
+
+    public void cappuccino(View view){
+
+    }
+
+    public void caramel(View view){
+
+    }
+
+    public void latte(View view){
+
+    }
+
+    public void coldBrew(View view){
+
+    }
+
+    public void mocha(View view){
+
+    }
+
+    public void turkishCoffee(View view){
+
+    }
+}
---
+++ app/src/main/java/com/alperentopcu/cafemenuapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.alperentopcu.cafemenuapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView); // Görünüm sınıfında, findViewById olarak tanımlanan bir yöntem, bir şey kimliğine sahip olan görünümü bulur.
+
+
+        Intent intent = new Intent();
+    }
+
+    public void coffees(View view){
+        Intent intent = new Intent(getApplicationContext(),CoffeesMenu.class);
+        startActivity(intent);
+    }
+
+    public void menu(View view){
+        // Intent
+    }
+
+    public void other(View view){
+        // Intent
+    }
+
+
+}
