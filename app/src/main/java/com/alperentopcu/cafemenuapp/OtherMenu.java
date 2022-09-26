54ebca6fc478a3acb6c355e2285696462bccdbff
Alperen Topcu <alperenttpc78@gmail.com>
2022-09-27 02:00:47 +0300

    Added warning messages and toast messages for OtherMenu.

--- app/src/main/java/com/alperentopcu/cafemenuapp/OtherMenu.java
+++ app/src/main/java/com/alperentopcu/cafemenuapp/OtherMenu.java
@@ -1,11 +1,14 @@
 package com.alperentopcu.cafemenuapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OtherMenu extends AppCompatActivity {
 
@@ -21,7 +24,7 @@ public class OtherMenu extends AppCompatActivity {
     }
 
     public void waiter(View view){
-
+        Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
     }
 
     public void pay(View view){
@@ -30,12 +33,29 @@ public class OtherMenu extends AppCompatActivity {
 
     public void rate(View view){
 
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("COFFEE SHOP");
+        alert.setMessage("Do you like it?");
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Please give feedback to our waiters", Toast.LENGTH_LONG).show();
+            }
+        });
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Thanks !", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alert.show();
+
     }
 
     public void backPage(View view){
-
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
-
     }
 }
