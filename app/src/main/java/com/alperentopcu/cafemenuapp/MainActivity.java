93e474b451d3a9d00078b2c9bb45c6e45e07ced9
Alperen Topcu <alperenttpc78@gmail.com>
2022-10-01 00:30:41 +0300

    Menu list - toast message added.

--- app/src/main/java/com/alperentopcu/cafemenuapp/MainActivity.java
+++ app/src/main/java/com/alperentopcu/cafemenuapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void menu(View view){
-        // Intent
+        Toast.makeText(getApplicationContext(), "Coming Soon ...", Toast.LENGTH_LONG).show();
     }
 
     public void other(View view){
